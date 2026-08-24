93a44581283c0493fba0bfa010f6ec77bb7ede05
Veronicayro <vrodorte@emeal.nttdata.com>
2026-08-24 09:38:02 -0500

    Update migrar-repositorios.sh

--- scripts/migrar-repositorios.sh
+++ scripts/migrar-repositorios.sh
@@ -1,1 +1,81 @@
+#!/bin/bash
+
+clear
+
+read -p "Por favor, introduce el valor del token para gitlab: " TOKEN_GITLAB
+read -p "Si desea repositorios de un grupo especifico ingresar el nombre aqui: " GROUP_GITLAB
+read -p "Por favor, introduce el valor del token para github: " TOKEN_GITHUB
+read -p "Por favor, introduce el valor de la organizacion para github: " OWNER
+#echo "El Token para Gitlab es: $TOKEN_GITLAB"
+#echo "El Token para Github es: $TOKEN_GITHUB"
+#echo "La organizacion para Github es: $OWNER"
+BUSCAR_GITLAB="localhost"
+BUSCAR_GITHUB="github.com"
+ruta_inicial=$(pwd)
+
+if [ -z "$GROUP_GITLAB" ]; then
+    ulr_gitlab="https://gitlab.com/api/v4/projects"
+else
+    ulr_gitlab="https://gitlab.com/api/v4/groups/$GROUP_GITLAB/projects"
+fi
+
+
+#REVISAR DOCUMENTACIÓN POR SI SE NECESITA FILTROS https://docs.gitlab.com/api/projects/
+curl --header "PRIVATE-TOKEN: ${TOKEN_GITLAB}" "$ulr_gitlab" | jq -c '.[] | {name, http_url_to_repo, description}' | while read -r elemento; do
+    # Extraer campos de cada elemento
+    repositorio=$(echo "$elemento" | jq -r '.name')
+    url_repo_gitlab=$(echo "$elemento" | jq -r '.http_url_to_repo')
+    description=$(echo "$elemento" | jq -r '.description')
+
+    [ -d "./migracion" ] && rm -rf "./migracion"
+    mkdir ./migracion && cd ./migracion
+
+    HTTP_CODE=$(curl -o /dev/null -s -w "%{http_code}" \
+        -H "Authorization: token $TOKEN_GITHUB" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/repos/${OWNER}/${repositorio}")
+
+    REEMPLAZAR="root:$TOKEN_GITLAB@localhost:8080"
+    url_repo_gitlab=${url_repo_gitlab/${BUSCAR_GITLAB}/${REEMPLAZAR}}
+    
+    if [[ "$HTTP_CODE" == "200" ]]; then
+        echo "EL REPOSITORIO EXISTE EN GITHUB Y SE VA A SOBREESCRIBIR"
+    else
+        echo "EL REPOSITORIO NO EXISTE EN GITHUB Y SE VA CREAR"
+        curl -X POST \
+            -H "Authorization: token $TOKEN_GITHUB" \
+            -H "Accept: application/vnd.github.v3+json" \
+            https://api.github.com/orgs/$OWNER/repos \
+            -d "{\"name\":\"$repositorio\", \"description\":\"$description\", \"private\":true}"
+
+        echo "SE CREO EL REPOSITORIO"
+    fi
+
+    echo "BUSQUEDA DE REPOSITORIO"
+    url_repo_github=$(curl -H "Authorization: token $TOKEN_GITHUB" \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/repos/${OWNER}/${repositorio}" | jq -r '.clone_url')
+
+    # 1. Clonar el repo de GitLab como espejo
+    git clone --mirror $url_repo_gitlab
+
+    # 2. Entrar en el directorio clonado
+    cd "$repositorio.git"
+    ls -a
+
+    # 3. Cambiar el remote a GitHub
+    REEMPLAZAR="$TOKEN_GITHUB@github.com"
+    url_repo_github=${url_repo_github/${BUSCAR_GITHUB}/${REEMPLAZAR}}
+
+    #https://github.com/prmrOrganizacion2/segundo-proyecto    
+    #git remote set-url origin https://github.com/user/repo2.git
+    echo "EJECUCION DEL REMOTE"
+    git remote set-url origin $url_repo_github
+
+    # 4. Hacer push de todo al nuevo destino
+    echo "EJECUCION DEL PUSH"
+    git push --mirror origin
+
+    cd $ruta_inicial
+done
 
